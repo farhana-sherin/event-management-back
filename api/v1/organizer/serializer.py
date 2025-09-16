8e1491a56ae41ca05404bf7c5e0037fa8cafe01c
farhan sherin <farhana23sherin9023@gmail.com>
2025-09-16 15:07:00 +0530

    edited seialser.py of organizer

--- api/v1/organizer/serializer.py
+++ api/v1/organizer/serializer.py
@@ -16,22 +16,25 @@ class OrganizerSerializer(ModelSerializer):
         fields = ['id', 'user']
         
 
-
 class EventSerializer(ModelSerializer):
     class Meta:
         model = Event   
         fields = [
             'id',
             'title',
+            'short_description',  # Added short_description field
             'description',
             'category',
             'location',
-            'start_at',
-            'end_at',
+            'start_date',  # Change from start_at to start_date
+            'start_time',  # Change from start_at to start_time
+            'end_date',    # Change from end_at to end_date
+            'end_time',    # Change from end_at to end_time
             'price',
             'total_seats',
             'available_seats',
             'images',
+            'is_active',   # Add is_active if needed
         ]
 
 
