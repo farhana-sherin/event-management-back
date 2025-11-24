eb614c43d610d68f5cf8674c8597bdd15762e913
farhan sherin <farhana23sherin9023@gmail.com>
2025-11-24 19:34:50 +0530

    Lightweight recommendation system and remove heavy libs

--- EventManagment/settings.py
+++ EventManagment/settings.py
@@ -132,3 +132,30 @@ CSRF_TRUSTED_ORIGINS = [
     "https://event-motivoc-frontend.vercel.app",
     "https://event-management-back-1jat.onrender.com",
 ]
+
+
+
+
+# -----------------------------
+# AUTO CREATE SUPERUSER ON RENDER
+# -----------------------------
+import os
+from django.contrib.auth import get_user_model
+
+if os.environ.get("CREATE_SUPERUSER") == "1":
+    User = get_user_model()
+
+    email = "admin123@gmail.com"
+    username = "admin"   # required for your model
+    password = "1234"
+
+    if not User.objects.filter(email=email).exists():
+        User.objects.create_superuser(
+            email=email,
+            username=username,
+            password=password
+        )
+        print("Superuser created with email login!")
+    else:
+        print("Superuser already exists!")
+
--- create_superuser.py
+++ create_superuser.py
@@ -1,3 +1,6 @@
+# -----------------------------
+# AUTO CREATE SUPERUSER ON RENDER
+# -----------------------------
 import os
 from django.contrib.auth import get_user_model
 
@@ -5,15 +8,15 @@ if os.environ.get("CREATE_SUPERUSER") == "1":
     User = get_user_model()
 
     email = "admin123@gmail.com"
-    username = "admin123"
+    username = "admin"   # required for your model
     password = "1234"
 
-    if not User.objects.filter(username=username).exists():
+    if not User.objects.filter(email=email).exists():
         User.objects.create_superuser(
-            username=username,
             email=email,
+            username=username,
             password=password
         )
-        print("Superuser created!")
+        print("Superuser created with email login!")
     else:
         print("Superuser already exists!")
