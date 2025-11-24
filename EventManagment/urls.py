88dfb332ee409104f580930c1c305b3c79fa375a
farhan sherin <farhana23sherin9023@gmail.com>
2025-11-24 12:53:19 +0530

    Update the backend settings,

--- EventManagment/urls.py
+++ EventManagment/urls.py
@@ -1,26 +1,18 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home   # ADD THIS
 
 urlpatterns = [
+    path('', home),  # 👈 DEFAULT URL (root)
     path('admin/', admin.site.urls),
     
-    path('api/v1/customer/',include("api.v1.customer.urls",namespace='customer')),
+    path('api/v1/customer/', include("api.v1.customer.urls", namespace='customer')),
     path('api/v1/organizer/', include("api.v1.organizer.urls", namespace='organizer')),
-    path('api/v1/payment/',include("api.v1.payment.urls",namespace="payment")),
-    path('api/v1/admin/',include("api.v1.admin.urls",namespace="admin"))
-
-
-
-
-
-
-
-
-
+    path('api/v1/payment/', include("api.v1.payment.urls", namespace="payment")),
+    path('api/v1/admin/', include("api.v1.admin.urls", namespace="admin")),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += (
