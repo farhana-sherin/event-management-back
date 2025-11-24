914e7b6bbbacac1ad1d026ed329918870fb15488
farhan sherin <farhana23sherin9023@gmail.com>
2025-11-24 19:25:46 +0530

    Lightweight recommendation system and remove heavy libs

--- create_superuser.py
+++ create_superuser.py
@@ -1,13 +1,19 @@
 import os
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+if os.environ.get("CREATE_SUPERUSER") == "1":
+    User = get_user_model()
 
-email = "admin123@gmail.com"
-password = "1234"
+    email = "admin123@gmail.com"
+    username = "admin123"
+    password = "1234"
 
-if not User.objects.filter(email=email).exists():
-    User.objects.create_superuser(email=email, password=password)
-    print("Superuser created!")
-else:
-    print("Superuser already exists!")
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        print("Superuser created!")
+    else:
+        print("Superuser already exists!")
